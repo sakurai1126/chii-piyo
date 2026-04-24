87a98607516afb5c3cc59988ae1a2bd38d491482
shohei sakurai <s.sakurai1126@gmail.com>
2026-04-25 07:03:52 +0900

    feat: Spring Initializrにてプロジェクトの雛型を生成

---
+++ backend/src/test/java/link/s_repo/chii_piyo/ChiiPiyoApplicationTests.java
@@ -0,0 +1,13 @@
+package link.s_repo.chii_piyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiiPiyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
