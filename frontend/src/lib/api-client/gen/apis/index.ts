5b5703b5ae3802fef5de66907ea26d6d18c15b9b
shohei sakurai <74898936+sakurai1126@users.noreply.github.com>
2026-05-02 01:42:45 +0900

    feat: OpenAPI Generatorの設定 (#67)
    * fix: OpenAPIGeneratorとの名前の衝突を解消
    
    * feat: バックエンドのOpenAPI Generator設定
    
    * feat: フロントエンドのOpenAPI Generator設定
    
    * feat: フロントエンドのAPIコード生成
    
    * chore: バックエンドにて 生成コードを認識するようプラグインを追加

---
+++ frontend/src/lib/api-client/gen/apis/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+
+export * from "./AlbumManagementApi";
+export * from "./CareRecordManagementApi";
+export * from "./FavoriteManagementApi";
+export * from "./FirstRecordManagementApi";
+export * from "./GrowthRecordManagementApi";
+export * from "./MediaCommentManagementApi";
+export * from "./MediaManagementApi";
+export * from "./SharingGroupManagementApi";
+export * from "./TagManagementApi";
+export * from "./TrashManagementApi";
+export * from "./UserManagementApi";
+export * from "./WordRecordManagementApi";
