d972033d2e723c8fe08835b9cb53755f7dbfb1ff
shohei sakurai <s.sakurai1126@gmail.com>
2026-05-04 22:10:18 +0900

    chore: 不要なimportを削除

--- backend/src/main/java/link/s_repo/chii_piyo/service/UserSyncService.java
+++ backend/src/main/java/link/s_repo/chii_piyo/service/UserSyncService.java
@@ -7,8 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-
 import static link.s_repo.chii_piyo.repository.gen.UsersDynamicSqlSupport.cognitoUserId;
 import static org.mybatis.dynamic.sql.SqlBuilder.isEqualTo;
 
