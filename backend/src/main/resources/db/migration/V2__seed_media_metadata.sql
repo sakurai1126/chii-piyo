5c7961c906d14eaa42c72810c8bcb8d074d45037
shohei sakurai <s.sakurai1126@gmail.com>
2026-05-09 10:49:12 +0900

    chore: メディアメタデータ初期シードデータを追加

---
+++ backend/src/main/resources/db/migration/V2__seed_media_metadata.sql
@@ -0,0 +1,13 @@
+-- 共有範囲グループ
+INSERT INTO sharing_groups (name) VALUES ('家族全員');
+INSERT INTO sharing_groups (name) VALUES ('夫婦のみ');
+
+
+-- アルバム
+INSERT INTO albums (title) VALUES ('保育園');
+INSERT INTO albums (title) VALUES ('誕生日');
+
+-- タグ
+INSERT INTO tags (name) VALUES ('ハイハイ');
+INSERT INTO tags (name) VALUES ('ミルク');
+INSERT INTO tags (name) VALUES ('寝顔');
