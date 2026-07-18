426f2ae93c50b3491a2d66a2e14896cf556b7cf3
shohei sakurai <s.sakurai1126@gmail.com>
2026-07-18 11:43:25 +0900

    refactor: モーダルページの作りを統一

--- frontend/src/app/(main)/@modal/(.)media/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Modal } from "@/components/layout/Modal";
-
-export default function MediaModalLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <div className="relative z-100">
-      <Modal>{children}</Modal>
-    </div>
-  );
-}
